35b9f4c11c4f2c6073164fb2dd3ad682c5aeb12d
Sonya Novikova <sona.nn@yandex.ru>
2024-10-01 00:53:40 +0300

    Some changes

--- 1_week/Task_1/Task_1/Task_1.py
+++ 1_week/Task_1/Task_1/Task_1.py
@@ -1,18 +1,19 @@
 # 8. String to Integer (atoi)
 # https://leetcode.com/problems/string-to-integer-atoi/description/?envType=problem-list-v2&envId=string
 
+
 def myAtoi(s):
     res = ""
     i = 0
     while i < len(s):
         if s[i].isdigit():
             res += s[i]
-        elif (len(res) != 0 and s[i] == ' '):
+        elif len(res) != 0 and s[i] == " ":
             break
-        elif (s[i] != ' ') and (s[i].isdigit() != True):
-            if (len(res) != 0 and (s[i] == '+' or s[i] == '-')):
+        elif (s[i] != " ") and (not (s[i].isdigit())):
+            if len(res) != 0 and (s[i] == "+" or s[i] == "-"):
                 break
-            elif len(res) == 0 and (s[i] == '-' or s[i] == '+'):
+            elif len(res) == 0 and (s[i] == "-" or s[i] == "+"):
                 res += s[i]
             else:
                 break
@@ -20,13 +21,14 @@ def myAtoi(s):
     if (len(res) == 0) or (res == "+") or (res == "-"):
         return 0
     else:
-        if int(res) <= -2 ** 31:
-            return -2 ** 31
-        elif int(res) >= 2 ** 31 -1:
-            return 2 ** 31 - 1
+        if int(res) <= -(2**31):
+            return -(2**31)
+        elif int(res) >= 2**31 - 1:
+            return 2**31 - 1
         else:
             return int(res)
-    
+
+
 s = input()
 print(myAtoi(s))
 
@@ -58,4 +60,3 @@ class Solution:
             else:
                 return int(res)
 """
-            
