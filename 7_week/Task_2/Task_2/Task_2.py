f31046fe73ed4447043d6c7dd357cdfbb942d282
Sonya Novikova <sona.nn@yandex.ru>
2024-11-23 01:53:41 +0300

    Add some comments in Task_2 in 7 week

--- 7_week/Task_2/Task_2/Task_2.py
+++ 7_week/Task_2/Task_2/Task_2.py
@@ -22,3 +22,22 @@ def numSubarrayProductLessThanK(nums, k):
 nums = [10, 5, 2, 6]
 k = 100
 print(numSubarrayProductLessThanK(nums, k))
+
+""" My solution in LeetCode
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+        res = 0
+        start = 0
+        end = 0
+        mult = 1
+        while end <= len(nums) - 1:
+            mult *= nums[end]
+            while mult >= k:
+                mult /= nums[start]
+                start += 1
+            res += end - start + 1
+            end += 1
+        return res
+"""
